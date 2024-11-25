b1ab5ed9ac6b2f9363a7886912bd886568e603bb
ConC <Con.CCC@Gmail.com>
2024-11-25 16:02:03 +1100

    Initial Commit

---
+++ FileMakerExpenseApp/FileMakerExpenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileMakerExpenseAppApp.swift
+//  FileMakerExpenseApp
+//
+//  Created by Con Coucoumakis on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FileMakerExpenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
